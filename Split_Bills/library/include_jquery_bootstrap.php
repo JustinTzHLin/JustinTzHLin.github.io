85a198e22c538e8ac397215965a02ffc9c4e3a94
Justin Lin <hozion612356@gmail.com>
2023-07-26 17:09:28 +0800

    recovery_2

---
+++ Split_Bills/library/include_jquery_bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+	//jquery相關
+	echo '<script type="text/javascript" src="library/jquery_library/jquery-3.6.0.js"></script>';
+	echo '<script type="text/javascript" src="library/jquery_library/jquery-ui-1.13.0.custom/jquery-ui.min.js"></script>';
+	echo '<link rel=stylesheet type="text/css" href="library/jquery_library/jquery-ui-1.13.0.custom/jquery-ui.css">';
+
+	//bootstrap相關
+	echo '<script type="text/javascript" src="library/bootstrap_library/js/bootstrap.js"></script>';
+	echo '<link rel=stylesheet type="text/css" href="library/bootstrap_library/css/bootstrap.css">';
+?>
